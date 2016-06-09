c7104955c12e9f78c0593c0b3d0f66b198670233
Elisha <eparslow18@my.whitworth.edu>
2016-06-08 21:15:18 -0700

    HW3 Complete

---
+++ HW03_01 Exception Handling/readint.cpp
@@ -0,0 +1,42 @@
+#include "readint.h"
+#include <stdexcept>
+#include <iostream>
+using namespace std;
+
+int read_int(const string &prompt, int low, int high)
+{
+	cin.exceptions(ios::failbit | ios::badbit);//only need this once 
+	int numInR = 0;								   //check low and high
+	if (low >= high)
+	{
+		throw (invalid_argument("Low is greater than or equal to high."));
+	}
+
+	while (true) {
+		try {
+			cout << prompt;
+			cin >> numInR;
+
+			if (numInR <= low || numInR >= high)//check numInR is between low and high
+			{
+				throw range_error("Num entered is outside of the range:");
+			}
+			return numInR;//similar to setting while loop to false and exits 
+		}
+		catch (const range_error& e) {
+			cout << "The number entered was out of range. " << endl;
+
+		}
+
+		catch (ios_base::failure &ex) {//exception if the user enters a non interger value
+			cout << "You entered an invalid input. " << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<int>::max(), '\n');
+		}
+	}
+
+
+	//rangeerror from library
+
+
+}
